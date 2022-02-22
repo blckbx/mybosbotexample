44ee7b17411875d4c654a5972c9eaf6a7fdf0f84
osito <74455114+blckbx@users.noreply.github.com>
2022-02-22 08:29:10 +0100

    fix method telegramLog

--- index.js
+++ index.js
@@ -2402,6 +2402,7 @@ routing rewards: ${median(forwardsAll.map(f => f.fee_mtokens / 1000.0), { pr: 1 
 
 // uses telegram logging if available
 const telegramLog = async message => {
+  const { token, chat_id } = mynode.settings?.telegram || {}
   var proxy = ''
   if(TELEGRAM_PROXY_HOST != '' && TELEGRAM_PROXY_PORT != '') { proxy = `socks://${TELEGRAM_PROXY_HOST}:${TELEGRAM_PROXY_PORT}` }
   if (token && chat_id) await bos.sayWithTelegramBot({ token, chat_id, message, proxy })
